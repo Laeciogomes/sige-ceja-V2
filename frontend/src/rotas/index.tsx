2c6f81ee468d1c9c2a9a641f7610b172d4a73b97
Laeciogomes <85498892+Laeciogomes@users.noreply.github.com>
2025-11-28 14:21:05 -0300

    Atualiza AppRoutes removendo travamento por carregando

--- frontend/src/rotas/index.tsx
+++ frontend/src/rotas/index.tsx
@@ -36,10 +36,19 @@ export const AppRoutes: React.FC = () => {
         <Route path="professores" element={<PaginaSimples titulo="Professores" />} />
         <Route path="coordenacao" element={<PaginaSimples titulo="Coordenação" />} />
         <Route path="direcao" element={<PaginaSimples titulo="Direção" />} />
-        <Route path="administracao" element={<PaginaSimples titulo="Administração" />} />
+        <Route
+          path="administracao"
+          element={<PaginaSimples titulo="Administração" />}
+        />
         <Route path="alunos" element={<PaginaSimples titulo="Alunos" />} />
-        <Route path="salas" element={<PaginaSimples titulo="Salas de Atendimento" />} />
-        <Route path="atendimentos" element={<PaginaSimples titulo="Atendimentos" />} />
+        <Route
+          path="salas"
+          element={<PaginaSimples titulo="Salas de Atendimento" />}
+        />
+        <Route
+          path="atendimentos"
+          element={<PaginaSimples titulo="Atendimentos" />}
+        />
         <Route path="relatorios" element={<PaginaSimples titulo="Relatórios" />} />
         <Route path="config" element={<PaginaSimples titulo="Configurações" />} />
       </Route>
